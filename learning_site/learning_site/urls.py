6a50b5956447b2fc84e619ad6ca1d2b27a8463c4
mikethebro <xenophon.of.athens@gmail.com>
2017-07-17 21:51:15 -0600

    adding a view for courses

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
+	url(r'^courses/', include('courses.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.hello_world),
 ]
